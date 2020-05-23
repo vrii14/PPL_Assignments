aba075700adc8c7ad5ef1df8d0c2161705389696
Vrinda Ahuja <56122560+vrii14@users.noreply.github.com>
2020-05-23 13:50:49 +0530

    Update try_except.py

--- Assignment_5/try_except.py
+++ Assignment_5/try_except.py
@@ -6,47 +6,50 @@ b = int(input())
 try:
 	print(a/b)
 except ZeroDivisionError:
-	print("A number cannot be divided by zero")
+	print("Zero Division Error:- A number cannot be divided by zero\n")
 
 
 
-#Type error since the input is of type string and we need it to be integer
+#Type error 
+#Since the input is of type string and we need it to be integer
 
 print("Enter a numeric string")
 a = input()
 try:
 	print(a/3)
 except TypeError:
-	print("A string cannot be divided by a integer. Convert the type of string to integer")
+	print("A string cannot be divided by a integer. Convert the type of string to integer\n")
 
 
 
 #If you don't know which type of exception to raise, then don't specify the exception name
+def fileopen():
+	while True:
+		print("Enter a file you want to read")
+		a = input()
+		try:
+			f = open(a, "r")
+			m = f.read()
+			print("This is the content of your file")
+			print(m)
+		except:
+			print("This file is not present in the current directory")
+			continue
+		else:
+			break
 
-print("Enter a file you want to read")
-a = input()
-try:
-	f = open(a, "r")
-	m = f.read()
-	print("This is the content of your file")
-	print(m)
-except:
-	print("This file is not present in the current directory")
-finally:
-	print("Finally executes like a common print statement.")
-
-
+fileopen()
 
 #Error when do not have a some permissions for the files (IO exception)
 
 try:
-    f = open('testfile','r')
-    f.write('Test write this')
+	f = open('test.txt','r')
+	f.write('Test write this')
 except IOError:
-    print("Error: Could not find file or read data")
+	print("\nError: Could not find file or read data\n")
 else:
-    print("Content written successfully")
-    f.close() 
+	print("Content written successfully")
+	f.close() 
 
 
 
@@ -57,7 +60,7 @@ print("I am trying to raise my exception")
 try:
 	raise Myerror("I deliberately raised this exception")
 except:
-	print("Myerror is not a defined exception")
+	print("Myerror is not a defined exception\n")
 
 
 
@@ -65,17 +68,17 @@ except:
 #we can also create functions using try except block. And it will run until user provides the expected input
 
 def ask():
-    
-    while True:
-        try:
-            n = int(input('Input an integer: '))
-        except:
-            print('An error occurred! Please try again!')
-            continue
-        else:
-            break
-            
-        
-    print('Thank you, your number squared is: ',n**2)
+	
+	while True:
+		try:
+			n = int(input('Input an integer: '))
+		except:
+			print('An error occurred! Please try again!')
+			continue
+		else:
+			break
+			
+		
+	print('Thank you, your number squared is: ',n**2)
 
 ask()
