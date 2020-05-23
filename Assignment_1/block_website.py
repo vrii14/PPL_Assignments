d149707c33f03359beee8080423e054dc8bcdb0b
Vrinda Ahuja <56122560+vrii14@users.noreply.github.com>
2020-05-23 13:49:19 +0530

    Update block_website.py

--- Assignment_1/block_website.py
+++ Assignment_1/block_website.py
@@ -1,9 +1,9 @@
 print("enter the website u want to block")
 a = input()
 ip = "127.0.0.1"
-host = "/etc/hosts"
+host = "C:/Windows/System32/drivers/etc/hosts"
 file1 = open(host, "r+")
-file1.truncate()
+
 website_list = file1.read()
 if a in website_list:
 	pass
