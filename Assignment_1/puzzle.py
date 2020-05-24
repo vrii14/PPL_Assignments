170113197911820582959b1d13709783d7859ae0
Vrinda Ahuja <56122560+vrii14@users.noreply.github.com>
2020-05-24 19:34:15 +0530

    Update puzzle.py

--- Assignment_1/puzzle.py
+++ Assignment_1/puzzle.py
@@ -1,26 +1,30 @@
-import random
-bank1 = [ "Tiger", "Grass", "Goat"]
-bank2 = []
-boat = []
-n = 0
-while len(bank1) != 0:
-	a =  random.randint(0, len(bank1) - 1)
-	p = bank1[a]
-	bank1.remove(p)
-	if "Tiger" in bank1 and "Goat" in bank1:
-		bank1.append(p)
-		continue
-	if "Goat" in bank1 and "Grass" in bank1:
-		bank1.append(p)
-		continue
-	
-	if "Tiger" in bank2 and "Goat" in bank2:
-		bank1.append(bank2.pop())
-		continue
-	if "Goat" in bank2 and "Grass" in bank2:
-		bank1.append(bank2.pop())
-		continue
-	bank2.append(p)
-	n += 1
-	print("In", n, "trip of boat man and", p)
-	
+b1 = {"grass", "goat", "tiger"}
+b2 = set()
+case1 = {"grass", "goat"}
+case2 = {"tiger", "goat"}
+b = 1
+while len(b1) != 0:
+    if b == 1:
+        if len(b1) == 3:
+            obj = b1.pop()
+            
+            if b1 != case1 and b1 != case2:
+                print(f"Farmer takes {obj} from bank1 to bank2")
+                b2.add(obj)
+                b = 2
+            else:
+                b1.add(obj)
+        else:
+            obj = b1.pop()
+            print(f"Farmer takes {obj} from bank1 to bank2")
+            b2.add(obj)
+            b = 2
+    else:
+        if b2 == case1 or b2 == case2:
+            obj = b2.pop()
+            print(f"Farmer takes {obj} from bank2 to bank1")
+            b1.add(obj)
+        else:
+            print("Farmer comes from bank2 to bank1")
+
+        b = 1
